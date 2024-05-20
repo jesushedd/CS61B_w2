49df9475b206624126cf62d1f1c3d5b72ee2cad2
jesushedd <jesushe99@gmail.com>
2024-05-20 10:52:36 -0500

    create SLList class

---
+++ SLList.java
@@ -0,0 +1,15 @@
+public class SLList {
+    public IntNode first;
+
+    public SLList(int x){
+        first = new IntNode(x, null);
+    }
+
+    public static void main(String[] args) {
+        /*Crates a list of one integer, 10 */
+        SLList L = new SLList(10);
+
+    }
+
+
+}
