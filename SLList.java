ba4c47652eda58b210bc2ea0c5a2859025fed108
jesushedd <jesushe99@gmail.com>
2024-05-20 11:28:31 -0500

    add private notion of Node

--- SLList.java
+++ SLList.java
@@ -1,13 +1,27 @@
 public class SLList {
-    public IntNode first;
+    //public int item;
+    // made it private to no mess with implementation from other classes
+    private IntNode first;
 
     public SLList(int x){
         first = new IntNode(x, null);
     }
 
+    public void addFirst(int num){
+        IntNode toAdd = new IntNode(num, first);
+        first = toAdd; 
+    }
+
+    public int getFirst(){
+        return first.item;
+    }
+
     public static void main(String[] args) {
         /*Crates a list of one integer, 10 */
         SLList L = new SLList(10);
+        L.addFirst(35);
+        L.addFirst(55);
+        System.out.println(L.getFirst());
 
     }
 
